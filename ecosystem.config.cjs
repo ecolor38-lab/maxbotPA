4f6de449446b89f813db68ea9debeba0b03898f4
ecolor38-lab <ecolor38@gmail.com>
2025-12-27 10:37:10 +0800

    fix: change port to 3001

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -8,13 +8,9 @@ module.exports = {
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'production',
-      PORT: 3000,
+      PORT: 3001,
       AUTO_START_SCHEDULER: 'true'
-    },
-    error_file: './logs/err.log',
-    out_file: './logs/out.log',
-    log_file: './logs/combined.log',
-    time: true
+    }
   }]
 };
 
